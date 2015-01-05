96f05436de663aa5c871e3560a00e31a58e159a7
Amunu <panyilinlove@qq.com>
2015-01-05 17:26:45 +0800

    css

--- app.js
+++ app.js
@@ -44,10 +44,10 @@ require('./routes/login')(app);
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-   res.status(err.status || 500);
-   res.render('error', {
-      message: err.message,
-      error: {}
-   });
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
 });
 
