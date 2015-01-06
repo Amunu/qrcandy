0596165f09b1cca829bd3732151850a430785239
Amunu <panyilinlove@qq.com>
2015-01-06 11:05:07 +0800

    删除二维码操作界面

--- routes/qrcode.js
+++ routes/qrcode.js
@@ -24,7 +24,14 @@ module.exports = function(app){
   });
 
   app.delete('/qrcode', function(req, res) {
-  	res.render('qrcode', {title : 'qrcode'});
+    if(!session.user) return res.send(400, {error: '请先<a href="/login">登录</a>'});
+    if(!req.body.qrcode_id) return res.send(400, {error: 'qrcode not null'});
+    qrcodeService.deleteQrcode({
+      qrcode_id : req.body.qrcode_id
+    }, function(err, data) {
+      if(err) return res.send(500, {error: err});
+      return res.send(200, {data: data});
+    });
   });
 
 }
